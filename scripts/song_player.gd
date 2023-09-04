67df5045a19232a517701387bf17242f0eccf0c8
fossesv17 <ossesvfabian@gmail.com>
2023-09-03 22:30:46 -0300

    get song position

--- scripts/song_player.gd
+++ scripts/song_player.gd
@@ -1,12 +1,20 @@
 extends AudioStreamPlayer2D
 
-var song = preload("res://sound/song.mp3")
+@export var bpm := 161
+var sec_per_beat = 60.0 / 60
+var song_pos = 0.0
+var ref_beat = 0
+var offset = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	stream = song
+	ref_beat = 0
+	sec_per_beat = 60.0 / bpm
 	play()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if playing:
+		song_pos = get_playback_position() + AudioServer.get_time_since_last_mix()
+		song_pos -= AudioServer.get_output_latency()
