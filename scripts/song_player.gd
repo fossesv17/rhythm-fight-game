4841cd611c2ab1c44560d6fdf543de49deb05dfd
fossesv17 <ossesvfabian@gmail.com>
2023-09-03 21:11:30 -0300

    added background

--- scripts/song_player.gd
+++ scripts/song_player.gd
@@ -1,12 +1,11 @@
 extends AudioStreamPlayer2D
 
-var song = preload("res://sound/survival-epic.mp3")
+var song = preload("res://sound/song.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var songPlayer = self
-	songPlayer.stream = song
-	songPlayer.play()
+	stream = song
+	play()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
