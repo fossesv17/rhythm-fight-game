9f789ffb6f150f46467d32fd51d4486803166e83
fabs17 <ossesvfabian@gmail.com>
2023-10-12 17:47:18 -0300

    vamos CHILEEE

--- scripts/song_player.gd
+++ scripts/song_player.gd
@@ -1,19 +1,72 @@
 extends AudioStreamPlayer2D
 
 @export var bpm := 99
+@export var measures := 4
+
+# track song pos and beats
 var sec_per_beat = 60.0 / 60
 var song_pos = 0.0
+var song_pos_in_beats = 1
 var ref_beat = 0
 var offset = 0
+var measure = 1
+
+# track song offset by technical stuff
+var closest = 0
+var time_off_beat = 0.0
+
+signal beat(pos)
+signal meas(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ref_beat = 0
 	sec_per_beat = 60.0 / bpm
-	play()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	if playing:
 		song_pos = get_playback_position() + AudioServer.get_time_since_last_mix()
 		song_pos -= AudioServer.get_output_latency()
+		song_pos_in_beats = int(floor(song_pos / sec_per_beat)) + offset
+		_report_beat()
+		
+func _report_beat():
+	if ref_beat < song_pos_in_beats:
+		if measures < measure:
+			measure = 1
+		emit_signal("beat", song_pos_in_beats)
+		emit_signal("meas", measure)
+		ref_beat = song_pos_in_beats
+		measure += 1
+
+func play_with_offset(num):
+	offset = num
+	$StartTimer.wait_time = sec_per_beat
+	$StartTimer.start()
+	
+func closest_beat(nth):
+	closest = int(round((song_pos / sec_per_beat) / nth) * nth)
+	time_off_beat = abs(closest * sec_per_beat - song_pos)
+	return Vector2(closest, time_off_beat)
+
+
+func play_from_beat(beat, off):
+	play()
+	seek(beat * sec_per_beat)
+	offset = off
+	measure = beat % measures
+
+
+func _on_StartTimer_timeout():
+	song_pos_in_beats += 1
+	if song_pos_in_beats < offset - 1:
+		$StartTimer.start()
+	elif song_pos_in_beats == offset - 1:
+		$StartTimer.wait_time = $StartTimer.wait_time - (AudioServer.get_time_to_next_mix() + 
+														AudioServer.get_output_latency())
+		$StartTimer.start()
+	else:
+		play()
+		$StartTimer.stop()
+	_report_beat() 
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -7,7 +7,7 @@ var menu_pos = 0
 func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _on_start_btn_pressed():
