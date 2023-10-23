43eec315f5f772431257ebe65aa7288fb01df41c
fossesv17 <ossesvfabian@gmail.com>
2023-10-22 22:30:05 -0300

    it MOOOVES

--- scripts/beat_area.gd
+++ scripts/beat_area.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
+var good = false
+var perfect = false
+var curr_note = null
 
+
+signal movebybeat(input)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +14,39 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		$AnimatedSprite2D.frame = 1
+		$AnimatedSprite2D.self_modulate = Color(0.9, 0.77 ,0)
+		if perfect:
+			curr_note.queue_free()
+			emit_signal("movebybeat")
+		elif good:
+			emit_signal("movebybeat")
+			curr_note.queue_free()
+		else:
+			print("BRO U SUCK")
+	else:
+		$AnimatedSprite2D.frame = 0
+		$AnimatedSprite2D.self_modulate = Color(1, 1, 1)
+		print("BUH")
+
+func _on_enter_good_zone(area):
+	if area.is_in_group("beatbar"):
+		good = true
+		curr_note = area
+		
+func _on_enter_perfect_zone(area):
+	if area.is_in_group("beatbar"):
+		perfect = true
+
+func _on_exit_perfect_zone(area):
+	if area.is_in_group("beatbar"):
+		perfect = false
+
+func _on_exit_good_zone(area):
+	if area.is_in_group("beatbar"):
+		good = false
+		curr_note = null
+
--- scripts/bee.gd
+++ scripts/bee.gd
@@ -12,12 +12,14 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	move_and_slide()
 
+func _process_input():
 	# Handle Jump.
 	if Input.is_action_just_pressed("p1_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the Input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionx = Input.get_axis("p1_left", "p1_right")
 	var directiony = Input.get_axis("p1_up", "p1_down")
