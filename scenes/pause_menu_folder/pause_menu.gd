231ddafaa668472d7da5170ed82c36a6d5c8fb77
EnzoLp <enzo.lpez.pstor@gmail.com>
2023-12-11 03:14:05 -0300

    minor fix menu

--- scenes/pause_menu_folder/pause_menu.gd
+++ scenes/pause_menu_folder/pause_menu.gd
@@ -1,15 +1,13 @@
 extends MarginContainer
 
-@onready var resume_button: Button = %Resume_button
-@onready var retry_button: Button = %Retry_button
+
 @onready var menu_button: Button = %Menu_button
 @onready var exit_button: Button = %Exit_button
 
 @export var main_menu: PackedScene
 
 func _ready() -> void:
-	resume_button.pressed.connect(_on_resume_button_pressed)
-	retry_button.pressed.connect(_on_retry_button_pressed)
+
 	menu_button.pressed.connect(_on_menu_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
 	
@@ -20,11 +18,6 @@ func _input(event: InputEvent) -> void:
 		visible = !visible
 		get_tree().paused = visible
 	
-func _on_resume_button_pressed():
-	hide()
-	
-func _on_retry_button_pressed():
-	get_tree().reload_current_scene()
 	
 func _on_menu_button_pressed():
 	if not main_menu:
